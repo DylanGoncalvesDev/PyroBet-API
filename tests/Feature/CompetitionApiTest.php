7ccd84a95f4083efa14cff29947da3e79f66d13c
Dylan Goncalves <dylanphx93@gmail.com>
2026-09-16 11:14:39 +0200

    feat: add test for listing competitions

--- tests/Feature/CompetitionApiTest.php
+++ tests/Feature/CompetitionApiTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use App\Models\Competition;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 
 class CompetitionApiTest extends TestCase
@@ -13,5 +16,18 @@ class CompetitionApiTest extends TestCase
        $response = $this->getJson('/api/competitions');
 
        $response->assertStatus(401);
+    }
+
+    public function testUserCanListCompetitions(): void
+    {
+        $player = User::factory()->create(['role' => 'admin']);
+
+        $competition = Competition::factory()->create();
+
+        Passport::actingAs($player);
+
+        $response = $this->getJson('/api/competitions');
+
+        $response->assertStatus(200);
     }
 }
