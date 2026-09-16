4db8572031e4ab71a48a11a2809d899ae82be9eb
Dylan Goncalves <dylanphx93@gmail.com>
2026-09-16 10:36:47 +0200

    feat: add test for listing teams

--- tests/Feature/TeamsApiTest.php
+++ tests/Feature/TeamsApiTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use App\Models\Team;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 
 class TeamsApiTest extends TestCase
@@ -15,4 +18,16 @@ class TeamsApiTest extends TestCase
        $response->assertStatus(401);
     }
 
+     public function testUserCanListTeams(): void
+    {
+        $player = User::factory()->create(['role' => 'admin']);
+
+        $team = Team::factory()->create();
+
+        Passport::actingAs($player);
+
+        $response = $this->getJson('/api/teams');
+
+        $response->assertStatus(200);
+    }
 }
